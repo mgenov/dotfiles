ab0eb545c570ab2b79f62e77d45cc52e8ce216f9
Miroslav Genov <mgenov@gmail.com>
2026-05-08 14:51:53 +0300

    nvim: set tokyonight theme

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1017,20 +1017,16 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'Mofiqul/dracula.nvim',
+    'folke/tokyonight.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     config = function()
-      ---@diagnostic disable-next-line: missing-fields
-      require('dracula').setup {
+      require('tokyonight').setup {
+        style = 'moon',
         styles = {
-          comments = { italic = false }, -- Disable italics in comments
+          comments = { italic = false },
         },
       }
-
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'dracula'
+      vim.cmd.colorscheme 'tokyonight-moon'
     end,
   },
 
